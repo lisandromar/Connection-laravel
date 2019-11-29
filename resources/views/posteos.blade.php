c0f9499a7a1e9701ae3b95ef43120ec716f76352
Ramiro <ramiropacheco@outlook.com>
2019-11-29 17:22:30 -0300

    vista controlado y modelo posteos

---
+++ resources/views/posteos.blade.php
@@ -0,0 +1,46 @@
+
+
+
+@extends('layouts.app')
+@section('content')
+    <h2 class="text-center">Postear</h2>
+    <div class="container-fluid">
+        <div class="row mt-5">
+        <div class="col-lg-8 offset-lg-2">
+            <!-- @if (count($errors->all())>0)
+                <ul class="alert alert-danger">
+                    @foreach ($errors->all() as $error)
+                        <li>{{$error}} </li>
+                    @endforeach
+                </ul>
+            @endif -->
+
+            <form action="/guardarPelicula" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="form-group">
+                    <label for="nombrePelicula">Que estás escuchando?</label>
+                    <input type="text" class="form-control" name="title" id="nombrePelicula" value="">
+
+                </div>
+
+                <div class="form-group">
+                        <label for="poster">Carga tu Track</label>
+                        <input type="file" class="form-control" name="poster" id="poster" value="">
+                </div>
+                    
+                <button type="submit" class="btn btn-primary">Postear</button>
+            </form>
+            
+        </div>
+    </div>
+    </div>
+@endsection
+
+
+
+
+
+
+
+
+
